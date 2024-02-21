8a972885afce324635b88080082d58a39d0e74f8
Merve ÖZKAN <82080759+MRV-1@users.noreply.github.com>
2024-02-21 10:21:28 +0300

    Update item_based_recommender.py

--- item_based_recommender.py
+++ item_based_recommender.py
@@ -2,78 +2,55 @@
 # Item-Based Collaborative Filtering
 ###########################################
 
-# Veri seti: https://grouplens.org/datasets/movielens/
-
-"""
-Online bir film izleme platformu (kuzukuzu.tv) iş birlikçi filtreleme yöntemi ile bir öneri sistemi geliştirmek istemektedir.
-İçerik temelli öneri sistemlerini deneyen şirket topluluğun kanaatlerini barındıracak şekilde öneriler geliştirmek istemektedir.
-Kullanıcılar bir film beğendiğinde o film ile benzer beğenilme örüntüsüne sahip olan diğer filmler önerilmek istenmektedir.
-
-veri seti hikayesi
-MovieLens isimli firma tarafından sağlanmıştır.
-İçerisinde filmler ve bu filmlere verilen puanları barındırmaktadır.
-Veri seti yaklaşık 27000 film için yaklaşık 2000000 derecelendirme içermektedir.
-
-"""
-
-
-
-# Adım 1: Veri Setinin Hazırlanması
-# Adım 2: User Movie Df'inin Oluşturulması
-# Adım 3: Item-Based Film Önerilerinin Yapılması
-# Adım 4: Çalışma Scriptinin Hazırlanması
+# Step 1: Preparing the Data Set
+# Step 2: Creating User Movie Df
+# Step 3: Making Item-Based Movie Recommendations
+# Step 4: Preparing the Working Script
 
 ######################################
-# Adım 1: Veri Setinin Hazırlanması
+# Step 1: Preparing the Data Set
 ######################################
+
 import pandas as pd
 pd.set_option('display.max_columns', 500)
-movie = pd.read_csv( r'C:\Users\MerveATASOY\Desktop\data_scientist_miuul\egitim_teorik_icerikler\Bolum_6_Tavsiye_Sistemleri\dataset\movie_lens_dataset\movie.csv')
-rating = pd.read_csv(r'C:\Users\MerveATASOY\Desktop\data_scientist_miuul\egitim_teorik_icerikler\Bolum_6_Tavsiye_Sistemleri\dataset\movie_lens_dataset\rating.csv')
+movie = pd.read_csv( r'dataset\movie_lens_dataset\movie.csv')
+rating = pd.read_csv(r'dataset\movie_lens_dataset\rating.csv')
 df = movie.merge(rating, how="left", on="movieId")
 df.head()
 
 
 ######################################
-# Adım 2: User Movie Df'inin Oluşturulması
+# Step 2: Creating User Movie Df
 ######################################
-# temel amaç bu adım
-
-# bir kullanıcı sadece bir filme puan vermiş olsun bu durumda user_movie_df'de bütün filmler için bir hücre teşkil edecektir
-# bu durum hesaplama işlemlerinde gecikmeye ve performans problemlerine sebep olmaktdır
-# hem bu duruma çözüm  hemde kullanıcıların hiç izlemedikleri filmlere film önermeyeceklerinden dolayı yapılması gereken bazı indirgeme işlemleri vardır
-# 1000'den az sayıda rate almış filmleri çalışmanın dışından bırakmak çözüm olabilir
-
 
 df.head()
-df.shape    # yaklaşık 20 milyon yorum var
+df.shape    
+df["title"].nunique()    
 
-df["title"].nunique()    # toplam 27 bin küsür film var
+# Question: There is a movie that got 10,000 comments, there is a movie that got 30 comments, should I really focus on all of them?
 
-# soru : 10.000 yorum alaan filmde var, 30 yorum alan filmde var hepsine odaklanmalı mıyım gerçekten?
-
-df["title"].value_counts().head()  #titleların value_counts'u hangi filmin kaç tane rate aldıkları bulunur
+df["title"].value_counts().head()  # The value_counts of the titles are used to find out which movie has the highest rate.
 
 
 comment_counts = pd.DataFrame(df["title"].value_counts())
-rare_movies = comment_counts[comment_counts["count"] <= 1000].index    #24103 film 1000dene az rate'e sahiptir
-common_movies = df[~df["title"].isin(rare_movies)]                     # bu df'de bu isimleri barındırmayanları getireceğim
+rare_movies = comment_counts[comment_counts["count"] <= 1000].index   
+common_movies = df[~df["title"].isin(rare_movies)]                  
 common_movies.shape
 common_movies["title"].nunique()
 df["title"].nunique()
 
-#gelen bilgiler üzerinde öyle bir işlem yapılmalı ki satırlarda kullanıcılar, sutunlarda ise title'lar olsun
+# An operation should be performed on the incoming information so that there are users in the rows and titles in the columns.
 
 user_movie_df = common_movies.pivot_table(index=["userId"], columns=["title"], values="rating")
-#pivot_table indexe hangi değişken gelecek, sutuna hangi değişken gelecek, bunların kesişimine ne gelecek sorularına olan cevaba ait verileri alır
 
+# Pivot table; It receives the data regarding the answer to the questions "Which variable will come to the index, which variable will come to the column, and what will come to the intersection of these?"
 
 user_movie_df.shape  #(138493, 3159)  (user, film)
 user_movie_df.columns
 
 
 ######################################
-# Adım 3: Item-Based Film Önerilerinin Yapılması
+# Step 3: Making Item-Based Movie Recommendations
 ######################################
 
 movie_name = "Matrix, The (1999)"
@@ -87,21 +64,20 @@ movie_name = user_movie_df[movie_name]
 user_movie_df.corrwith(movie_name).sort_values(ascending=False).head(10)
 
 
-# girilen keyword'e ait tüm filmleri getirir
+# returns all movies belonging to the entered keyword
 def check_film(keyword, user_movie_df):
     return [col for col in user_movie_df.columns if keyword in col]
 
 check_film("Insomnia", user_movie_df)
 
-
 ######################################
-# Adım 4: Çalışma Scriptinin Hazırlanması
+# Step 4: Preparing the Working Script
 ######################################
 
 def create_user_movie_df():
     import pandas as pd
-    movie = pd.read_csv(r'C:\Users\MerveATASOY\Desktop\data_scientist_miuul\egitim_teorik_icerikler\Bolum_6_Tavsiye_Sistemleri\dataset\movie_lens_dataset\movie.csv')
-    rating = pd.read_csv(r'C:\Users\MerveATASOY\Desktop\data_scientist_miuul\egitim_teorik_icerikler\Bolum_6_Tavsiye_Sistemleri\dataset\movie_lens_dataset\rating.csv')
+    movie = pd.read_csv(r'dataset\movie_lens_dataset\movie.csv')
+    rating = pd.read_csv(r'dataset\movie_lens_dataset\rating.csv')
     df = movie.merge(rating, how="left", on="movieId")
     comment_counts = pd.DataFrame(df["title"].value_counts())
     rare_movies = comment_counts[comment_counts["count"] <= 1000].index
@@ -111,15 +87,12 @@ def create_user_movie_df():
 
 user_movie_df = create_user_movie_df()
 
-# verilen film ve diğer filmlerin beğenilme patternlerine bakar, ardından 10 tan corelasyonu yüksek film getirir
+# It looks at the liking patterns of the given movie and other movies, then brings 10 movies with high tan correlation
 def item_based_recommender(movie_name, user_movie_df):
     movie_name = user_movie_df[movie_name]
     return user_movie_df.corrwith(movie_name).sort_values(ascending=False).head(10)
 
 item_based_recommender("Matrix, The (1999)", user_movie_df)
-
-# Sonuçlar  geç dönüyor; db'ye sonuçlar aktarılmalı ve web sitesi arayüzünden önerilmesi gereken filmler indexler(db'de) vasıtasıyla hızlıca döndürülür
-
 
 movie_name = pd.Series(user_movie_df.columns).sample(1).values[0]
 
